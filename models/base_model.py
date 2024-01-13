fe8c8e85f15a9400b5a4a2ebeb49df19694540ed
Duaa Rabie <Duaarabie11@gmail.com>
2024-01-13 11:41:23 -0800

    update code

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,9 @@ class BaseModel():
     """ This is the base class """
     def __init__(self, *args, **kwargs):
         """ This is the constructor """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -16,10 +19,7 @@ class BaseModel():
                 if key != "__class__":
                     self.__setattr__(key, value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            
+            models.storage.new(self)
 
     def __str__(self):
         """ This is the instance representation method """
