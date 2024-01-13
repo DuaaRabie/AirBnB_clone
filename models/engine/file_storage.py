e38af0c81b6456da52bf211d319edcada92e578b
Duaa Rabie <Duaarabie11@gmail.com>
2024-01-13 11:11:33 -0800

    update code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,5 +28,5 @@ class FileStorage():
         """ This method reload instances from file to the dictionary"""
         if os.path.exists(self.__file_path):
             with open(self.__file_path, "r") as f:
-                self.__objects =
-                {k: BaseModel(**v) for k, v in json.load(f).items()}
+                self.__objects =\
+                        {k: BaseModel(**v) for k, v in json.load(f).items()}
