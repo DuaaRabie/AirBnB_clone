31f476953c43a81c48cd3de8887bec480de2fd5f
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-03 14:24:23 -0800

    update code

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,6 @@ class BaseModel():
                         #value, "%Y-%m-%dT%H:%M:%S.%f")
                     # also can use simple setattr()
                     setattr(self, key, value)
-                    self.save()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -33,7 +32,7 @@ class BaseModel():
 
     def save(self):
         """ This method update the updated_at attribute """
-        self.updated_at = datetime.now()
+        #self.updated_at = datetime.now()
         models.storage.save()
 
     def __setattr__(self, name, value):
