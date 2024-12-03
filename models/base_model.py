2bc334dd36db60e15dfd2c7f211665d3255c37b0
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-03 14:06:23 -0800

    update code

--- models/base_model.py
+++ models/base_model.py
@@ -14,14 +14,15 @@ class BaseModel():
                 if key != "__class__":
                     if key in ["created_at", "updated_at"]:
                         # also can use datetime.fromisoformat(value)
-                        value = datetime.strptime(
-                                value, "%Y-%m-%dT%H:%M:%S.%f")
+                        value = datetime.fromisoformat(value)
+                        #value = datetime.strptime(
+                        #value, "%Y-%m-%dT%H:%M:%S.%f")
                     # also can use simple setattr()
                     self.__setattr__(key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
@@ -31,13 +32,13 @@ class BaseModel():
 
     def save(self):
         """ This method update the updated_at attribute """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def __setattr__(self, name, value):
         """ This override __setattr__ method """
         if name != "updated_at":
-            self.updated_at = datetime.utcnow()
+            self.updated_at = datetime.now()
         super().__setattr__(name, value)
 
     def to_dict(self):
