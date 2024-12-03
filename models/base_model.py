f89b34b5f4d375f746e7926f7ba508333588de14
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-03 14:09:48 -0800

    update code

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel():
                         #value = datetime.strptime(
                         #value, "%Y-%m-%dT%H:%M:%S.%f")
                     # also can use simple setattr()
-                    self.__setattr__(key, value)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
