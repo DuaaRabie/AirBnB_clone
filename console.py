b03d6eba8c75212378c2396e0f901cad45aba6e5
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-03 16:29:47 -0800

    update code

--- console.py
+++ console.py
@@ -137,4 +137,7 @@ class HBNBCommand(cmd.Cmd):
 
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    try:
+        HBNBCommand().cmdloop()
+    except KeyboardInterrupt:
+        print()
