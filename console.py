19141202b7c2c0f2498567c341321f6e750e6248
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-06 05:40:27 -0800

    task10

--- console.py
+++ console.py
@@ -6,6 +6,11 @@ import sys
 from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.Amenity import Amenity
+from models.review import Review
 from models import storage
 
 
